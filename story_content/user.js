37985324b62705e30a80522b156c2ca9a3826131
senecasandbox <58563392+senecasandbox@users.noreply.github.com>
2021-06-08 14:17:00 -0400

    draft7

--- story_content/user.js
+++ story_content/user.js
@@ -2,7 +2,7 @@ function ExecuteScript(strId)
 {
   switch (strId)
   {
-      case "5iKqf3ieofm":
+      case "6ptjmuk10gT":
         Script1();
         break;
   }
@@ -40,7 +40,7 @@ overflow: visible !important;
 .slide-transition-container {
 overflow: visible !important;
 }
-@page {size: A4 portrait;max-height:99%; max-width:99%}
+@page {size: A4 portrait;max-height:80%; max-width:80%}
 .slide-container, .slide-container * {
 visibility: visible !important;
 margin-top: 0px !important;
