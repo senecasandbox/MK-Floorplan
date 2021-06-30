4372ae3221c6e440f69a159a46136dd32636f24f
senecasandbox <58563392+senecasandbox@users.noreply.github.com>
2021-06-30 08:35:56 -0400

    draft8

--- story_content/user.js
+++ story_content/user.js
@@ -2,7 +2,7 @@ function ExecuteScript(strId)
 {
   switch (strId)
   {
-      case "6l4YJR2Rrod":
+      case "5zZVJlC0l9e":
         Script1();
         break;
   }
@@ -10,90 +10,6 @@ function ExecuteScript(strId)
 
 function Script1()
 {
-  var sWidth = screen.width;
-console.log("sWidth "+sWidth);
-var ComputedStyle = getComputedStyle(document.body);
-console.log("CS "+ComputedStyle);
-var divW = ComputedStyle.width;
-var w = divW.replace(/[^0-9]/g,'');
-var wN = Number(w);
-console.log("Width "+wN);
-if (w > 1000) { 
-var styles = `@media print {
-body, * { visibility: hidden; }
-html, body { overflow: hidden; transform: translateZ(0); }
-#slide {
-transform: scale(0.5) !important;
-}
-#wrapper {
-transform: scale(0.5) !important;
-}
-#slide,
-#wrapper {
-width: 100% !important;
-height: 100% !important;
-overflow: visible !important;
-}
-#frame {
-overflow: visible !important;
-}
-.slide-transition-container {
-overflow: visible !important;
-}
-@page {size: A4 portrait;max-height:60%; max-width:60%}
-.slide-container, .slide-container * {
-visibility: visible !important;
-margin-top: 0px !important;
-margin-left: 0px !important;
-}
-#outline-panel {
-display: none !important;
-}
-}
-}`
-var stylesheet = document.createElement('style');
-stylesheet.type = 'text/css';
-stylesheet.innerText = styles;
-document.head.appendChild(stylesheet);
-} else {
-var styles = `@media print {
-body, * { visibility: hidden; }
-html, body { overflow: hidden; transform: translateZ(0); }
-#slide {
-transform: scale(1.0) !important;
-}
-#wrapper {
-transform: scale(1.0) !important;
-}
-#slide,
-#wrapper {
-width: 100% !important;
-height: 100% !important;
-overflow: visible !important;
-}
-#frame {
-overflow: visible !important;
-}
-.slide-transition-container {
-overflow: visible !important;
-}
-@page {size: A4 landscape;max-height:99%; max-width:99%}
-.slide-container, .slide-container * {
-visibility: visible !important;
-margin-top: 0px !important;
-margin-right: 10px !important;
-}
-#outline-panel {
-display: none !important;
-}
-}
-}`
-var stylesheet = document.createElement('style');
-stylesheet.type = 'text/css';
-stylesheet.innerText = styles;
-document.head.appendChild(stylesheet);
-}
-
-window.print();
+  window.print()
 }
 
